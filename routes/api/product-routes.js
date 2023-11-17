5e240398a8c6e8591db4ea2951dc9366864cc99b
libbyou <lcyou95@gmail.com>
2023-11-16 20:03:15 -0600

    added teh routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,37 @@
 const router = require('express').Router();
+const { error } = require('console');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-});
+  try {
+    const productData = await Product.findAll({
+      include: [{model: Category},
+      {model: Tag}]
+    });
+      res.status(200).json(productData);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+    });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}]
+    });
+    res.status(200).json(productData);
+  }
+  catch (error) { 
+    res.status(400).json(error);
+  }
 });
 
 // create new product
@@ -41,9 +60,9 @@ router.post('/', (req, res) => {
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
+    .catch((error) => {
+      console.log(error);
+      res.status(400).json(error);
     });
 });
 
@@ -88,12 +107,28 @@ router.put('/:id', (req, res) => {
     })
     .catch((err) => {
       // console.log(err);
-      res.status(400).json(err);
+      res.status(400).json(error);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://i4mdsdg4at5sdvt2:wbg0r5cn25cyb9s8@eyvqcfxf5reja3nv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/cvd6k2vjnp5om40d')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// app.listen(PORT, async () => {
+//   console.log(`App listening on port ${PORT}!`);
+//   try{
+//     await sequelize.authenticate();
+//   }catch(error){
+//     console.log('having error')
+//   }
+
+//   sequelize.sync({force: false});
+// });
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
